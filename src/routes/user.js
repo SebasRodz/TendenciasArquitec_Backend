4a8cf6e895ab38c995e71fa07db218bb6591de49
SebasRodz <sebasxiom@gmail.com>
2022-11-11 16:42:43 -0500

    Update rutas

--- src/routes/user.js
+++ src/routes/user.js
@@ -49,35 +49,5 @@ router.post("/auth", (req, res) => {
         }
     });
 });
-  
-  /*
-  // Obtener un usuario GET
-  router.get("/users/:id", (req, res) => {
-    const { id } = req.params;
-    userSchema
-      .findById(id)
-      .then((data) => res.json(data))
-      .catch((error) => res.json({ message: error }));
-  });
-  
-  // Eliminar un usuario DELETE
-  router.delete("/users/:id", (req, res) => {
-    const { id } = req.params;
-    userSchema
-      .remove({ _id: id })
-      .then((data) => res.json(data))
-      .catch((error) => res.json({ message: error }));
-  });
-  
-  // Actualizar un usuario PUT
-  router.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, age, email } = req.body;
-    userSchema
-      .updateOne({ _id: id }, { $set: { name, age, email } })
-      .then((data) => res.json(data))
-      .catch((error) => res.json({ message: error }));
-  });
-*/
 
 module.exports = router;
