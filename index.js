ae73c94cd531c28e8e8056f271f210ca3b48449b
jose.caicedo <practicas.0003@sesitdigital.com>
2022-11-12 01:23:22 -0500

    Add open API swagger

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // Developer branch
 const app = require("./src/app");
+const path = require("path");
+//Swagger UI (OPEN API)
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API",
+            version: "1.0.0",
+            description: "API",
+        },
+        servers: [{
+            url: "http://localhost:3000",
+        }],
+        },
+        apis: [`${path.join(__dirname, "src/routes/*.js")}`],
+    };
+
+//middleware
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerSpec)));
 
 // Settings
 app.set('port', process.env.PORT || 3000)
