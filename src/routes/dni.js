bcd5d7a3520ecf2f818d723db61ebb88bf2c1a25
SebasRodz <sebasxiom@gmail.com>
2022-12-02 17:46:11 -0500

    Cambiando la API DNI

--- src/routes/dni.js
+++ src/routes/dni.js
@@ -68,9 +68,11 @@ const fetch = require('node-fetch');
  */
 router.get('/pac/:dni', async (req, res) => {
     const {dni} = req.params
-    fetch(`https://citasenlinea.sisol.gob.pe/Account/FnGetPaciente?idTipoDocumento=1&nroDocumento=${dni}`)
+    //fetch(`https://citasenlinea.sisol.gob.pe/Account/FnGetPaciente?idTipoDocumento=1&nroDocumento=${dni}`)
+    fetch(`https://api.apis.net.pe/v1/dni?numero=${dni}`)
         .then(res => res.json())
         .then(response => res.json(response))
+        .catch(err => console.error('error:' + err));
 });
 
 module.exports = router;
